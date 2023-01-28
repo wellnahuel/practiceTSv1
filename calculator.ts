8b3d7013758d23834976cf17edccb7793e02f0e1
wellnahuel <nahuelcittadino@gmail.com>
2023-01-28 14:39:43 -0300

    pendiente ver porque me toma como NaN a las constantes a y b

--- calculator.ts
+++ calculator.ts
@@ -1,7 +1,15 @@
-type Operation = 'multiply' | 'add' | 'divide'
+type Operation = 'multiply' | 'add' | 'subtract' | 'divide';
 type Result = number | string   //el export es como en js con los modulos, puedo reutilizarlo donde quiera
+const operations = [ 'multiply', 'add', 'subtract', 'divide' ];
+
 const calculator = (a: number, b: number, op: Operation): Result => { //puedo elegir que es lo que quiero que devuelva, antes de la flecha van dos puntos 
+    if (!operations.includes(op)) {
+        console.log('This operation is undefined');
+    }
+
     if (op === 'multiply') return a * b
+    if (op === 'subtract') return a - b;
+
     if (op === 'add') return a + b
     if (op === 'divide') {
         if (b === 0) throw new Error('cant divide by 0!')
@@ -11,3 +19,6 @@ const calculator = (a: number, b: number, op: Operation): Result => { //puedo el
 }
 const result = calculator(1, 4, 'add')
 console.log(result)
+
+
+// El process.argv permite recuperar los arguments que pasamos
--- multiplier.ts
+++ multiplier.ts
@@ -1,6 +1,44 @@
-const multiplicator = (a: number ,b: number, printText: string) => {
-    console.log(printText, a * b) //los tipos los defino con dos puntos, tengo muchisimos más que los primitivos de JS (ver)
+const multiplicator = (a: number, b: number, printText: string) => {
+    console.log(printText, a * b); //los tipos los defino con dos puntos, tengo muchisimos más que los primitivos de JS (ver)
 }
 
-multiplicator(2 ,4 , 'Multiplied numbers 2 and 4, the result is: ')
+multiplicator(2, 4, 'Multiplied numbers 2 and 4, the result is: ')
 //si en el primer parametro meto algo distinto a un numero, por ejemplo un string , ya el VSC sin correr el programa empieza a decirme que hay algo mal. 
+
+// Parsear argumentos
+const parseArguments = (args): number[] => { //en lugar de Array<number>, puede ir number[]...
+    if (args.length !== 4) throw new Error('Wrong number of arguments');
+
+    const firstNumber = Number(args[2]);
+    const secondNumber = Number(args[3]);
+
+    if (!isNaN(firstNumber) && !isNaN(secondNumber)) {
+        return [
+            firstNumber,
+            secondNumber
+        ]
+    }
+    throw new Error('Provided values were not numbers!!!')
+};
+
+console.log('1', process.argv) //la primer posicion es el ts-node , el ejecutor
+//la segunda posicion es el nombre del archivo, todo esto es lo del package.json
+
+/* [
+    'D:\\typescript-bootcamp\\node_modules\\ts-node\\dist\\bin.js',
+    'D:\\typescript-bootcamp\\multiplier.ts',
+    '3',
+    '4'
+] */
+
+const cleanArguments = parseArguments(process.argv);
+
+console.log('cleanArguments', cleanArguments)
+
+const a: number = Number(cleanArguments[0]); // Se está obligando a volverse un tipo número
+const b: number = Number(cleanArguments[1]); // Se está obligando a volverse un tipo número
+console.log('2', process.argv)
+console.log('3', a)
+console.log('4', b)
+
+multiplicator(a, b, `Multiplied ${a} and ${b}, and the result is: `)
